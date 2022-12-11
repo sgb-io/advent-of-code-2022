2b8c99787c43b09ff7e336f82e3b05f4f6e1326f
sgb-io <sam@sgb.io>
2022-12-11 14:39:34 +0000

    Day 10 Part 1

--- src/day10/day10.ts
+++ src/day10/day10.ts
@@ -1,10 +1,69 @@
 import { parseRawData } from "../utils/parseRawData";
 
-(async () => {
-  const rawData = await parseRawData(__dirname, "test.txt");
-  console.log(rawData);
+const INTERESTING_CYCLES = [20, 60, 100, 140, 140, 180, 220];
 
-  const part1Answer = "todo";
+function listenToSignalStrength(rawData: string) {
+  const instructions = [...rawData.split("\n")];
+  const signalStrenghs: number[] = [];
+  let currentRegisterValue = 1;
+  let cycles = 0;
+  let nextValue: number | undefined = undefined;
+
+  while (instructions.length) {
+    cycles += 1;
+
+    if (INTERESTING_CYCLES.includes(cycles)) {
+      signalStrenghs.push(currentRegisterValue * cycles);
+    }
+
+    if (instructions[0] === "noop") {
+      // console.log(
+      //   "cycle",
+      //   cycles,
+      //   `(register value ${currentRegisterValue} unchanged - noop)`
+      // );
+      instructions.shift();
+      continue;
+    }
+
+    if (nextValue) {
+      // console.log(
+      //   "cycle",
+      //   cycles,
+      //   `${currentRegisterValue} => ${currentRegisterValue + nextValue}`
+      // );
+      currentRegisterValue = currentRegisterValue + nextValue;
+      nextValue = undefined;
+      instructions.shift();
+      continue;
+    }
+
+    if (instructions.length) {
+      const newValue = parseInt(instructions[0].split(" ")[1], 10);
+      nextValue = newValue;
+      // console.log(
+      //   "cycle",
+      //   cycles,
+      //   `(register value ${currentRegisterValue} unchanged, but nextValue has been set to ${nextValue})`
+      // );
+    }
+  }
+
+  console.log(cycles, "cycles,", "register value", currentRegisterValue);
+
+  return signalStrenghs;
+}
+
+(async () => {
+  const rawData = await parseRawData(__dirname, "input.txt");
+
+  const signalStrenghs = listenToSignalStrength(rawData);
+
+  // Test answer: 13140
+  const part1Answer = signalStrenghs.reduce(
+    (total, strength) => (total += strength),
+    0
+  );
   console.log("Part 1 Answer:", part1Answer);
 
   const part2Answer = "todo";
